a0bc1fd59a83265d7dadbcc756ef95d20330219a
jerekorhonenn <73354081+jerekorhonenn@users.noreply.github.com>
2023-01-15 01:30:47 +0200

    Removed raw json line

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
 streamlit.text(fruityvice_response)
 
 # take the json version of response and normalize it
